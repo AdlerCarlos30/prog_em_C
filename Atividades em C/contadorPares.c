3a0063a012c630d5c0c90bbb1a4529b5a2796928
AdlerCarlos30 <adler.carlos.18@gmail.com>
2025-07-10 11:23:07 -0400

    Create contadorPares.c

---
+++ Atividades em C/contadorPares.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+    int contador;
+    
+    //faz a contagem de 2 em 2
+    for(contador = 0; contador <= 20; contador+=2)
+{
+    printf("%d\n", contador);
+}
+    return 0;
+}
