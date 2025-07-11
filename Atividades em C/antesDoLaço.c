a0a8393ca4c75bce06e1577f051fbb79fdbffaf7
AdlerCarlos30 <adler.carlos.18@gmail.com>
2025-07-11 08:38:22 -0400

    Create antesDoLaço.c

---
+++ Atividades em C/antesDoLaço.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int contador;
+for(contador = 1; contador <= 10; contador++)
+{
+printf("\n%d\n", contador);
+}
+for(contador = 10; contador >= 0; contador--)
+{
+printf("\n%d\n", contador);
+}
+    return 0;
+}
