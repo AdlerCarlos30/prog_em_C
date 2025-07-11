5eb2583ee9c9a48c238c71f315dca9e1560308c0
AdlerCarlos30 <adler.carlos.18@gmail.com>
2025-07-11 08:38:16 -0400

    Create depoisDoLaço.c

---
+++ Atividades em C/depoisDoLaço.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int contador = 0;
+for(contador; contador <= 10; contador++)
+{
+printf("\n%d\n", contador);
+}
+for(contador; contador >= 0; contador--)
+{
+printf("\n%d\n", contador);
+}
+
+    return 0;
+}
