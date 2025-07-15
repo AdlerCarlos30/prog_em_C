95388dacdfdd7cea5cccefb43c1a73012ac78486
AdlerCarlos30 <adler.carlos.18@gmail.com>
2025-07-15 09:54:05 -0400

    .

--- Atividades em C/paresWhile.c
+++ Atividades em C/paresWhile.c
@@ -3,7 +3,7 @@
 int main() {
     int num, pares = 0, x =0;
 
-   do {
+    while (x < 10) {
         printf("Digite o %dº número: ", x + 1);
         scanf("%d", &num);
 
@@ -11,8 +11,8 @@ int main() {
             pares++;
         }
         x++;
-        
-    }   while (x < 10); 
+    }
+
     printf("Quantidade de números pares: %d\n", pares);
     
     return 0;
