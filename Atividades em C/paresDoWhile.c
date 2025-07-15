8242c0cd8cf5d26710066ae45a73f385ad2a1172
AdlerCarlos30 <adler.carlos.18@gmail.com>
2025-07-15 09:40:59 -0400

    Create paresDoWhile.c

---
+++ Atividades em C/paresDoWhile.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num, pares = 0, x =0;
+
+   do {
+        printf("Digite o %dº número: ", x + 1);
+        scanf("%d", &num);
+
+        if (num % 2 == 0) {
+            pares++;
+        }
+        x++;
+    }   while (x < 10); 
+    printf("Quantidade de números pares: %d\n", pares);
+    
+    return 0;
+}
