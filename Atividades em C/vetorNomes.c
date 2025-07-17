55268b1b78fe16a7eea5f5701923bda992df1e21
AdlerCarlos30 <adler.carlos.18@gmail.com>
2025-07-17 09:06:27 -0400

    Create vetorNomes.c

---
+++ Atividades em C/vetorNomes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char nomes [5] [150];
+    
+    for (int cont = 0; cont < 5; cont++) {
+        printf("Digite o nome %d: ", cont + 1);
+        fgets(nomes[cont], 150, stdin);
+        nomes[cont][strcspn(nomes[cont], "\n")] = '\0';
+        
+    }
+    
+    printf("\nNomes digitados:\n");
+    
+    for (int cont = 0; cont < 5; cont++) {
+        printf("\n%s\n", nomes[cont]);
+        
+    }
+    return 0;
+    
+}
