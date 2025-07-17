66724a14ee8649978324303e70b18c96b1d9d2c7
AdlerCarlos30 <adler.carlos.18@gmail.com>
2025-07-17 10:22:13 -0400

    Update vetorNomesQntd.c

--- Atividades em C/vetorNomesQntd.c
+++ Atividades em C/vetorNomesQntd.c
@@ -4,14 +4,15 @@
 int main() {
     int qntd;
     
-    printf("Quantos Nomes Você quer Cadastrar? ");
-    scanf("%d", &qntd);
-    getchar();
+     printf(" === Cadastro Nomes ===\n");
+     printf("\nQuantos Nomes Você quer Cadastrar? ");
+     scanf("%d", &qntd);
+     getchar();
     
     char nomes [qntd] [100];
     
     for (int cntd = 0; cntd < qntd; cntd++) {
-        printf("Digite o nome %d: ", cntd + 1);
+        printf("Digite o %d° Nome Completo: ", cntd + 1);
         fgets(nomes[cntd], 100, stdin);
         nomes[cntd][strcspn(nomes[cntd], "\n")] = '\0';
         
@@ -20,7 +21,7 @@ int main() {
     printf("\nNomes digitados:\n");
     
     for (int cntd = 0; cntd < qntd; cntd++) {
-        printf("\n%s\n", nomes[cntd]);
+        printf("\n%d°: %s\n", cntd + 1, nomes[cntd]);
         
     }
     return 0;
